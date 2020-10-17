e54fd572d3c4c4a3461622c81b90d4fb589bc831
Goodyeah <44746806+b0r3dd3v@users.noreply.github.com>
2020-10-17 23:40:11 +0300

    Update build.rs
    (*minor typhus*)

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() {
      cc::Build::new()
         .compiler("/usr/bin/clang")
         .no_default_flags(true)
-        .file("src//webwrapper.c")
+        .file("src/webwrapper.c")
         .pic(true)
         .static_flag(true)
         .opt_level(3)
