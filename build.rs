14d3fe32cbecd082661c7c99525d317a20e743c7
Goodyeah <44746806+b0r3dd3v@users.noreply.github.com>
2020-10-17 23:52:36 +0300

    Update build.rs
    It didn't even Mutter?

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ fn main() {
      cc::Build::new()
         .compiler("/usr/bin/clang")
         .no_default_flags(true)
-        .file("src/webwrapper.c")
+        .file("webwrapper.c")
         .pic(true)
         .static_flag(true)
         .opt_level(3)
-        .include("deps/include")
-        .compile("/tmp/libwebpwrapper.a");
+        .include("/usr/include/webp")
+        .compile("/tmp/lib/libwebpwrapper.a");
     println!("cargo:rustc-link-search=/tmp/lib");
 }
