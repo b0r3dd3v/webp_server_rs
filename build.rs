76c05f66170aaf8ba84ddda2ce0b4102b29cc54d
Goodyeah <44746806+b0r3dd3v@users.noreply.github.com>
2020-10-17 23:37:47 +0300

    Update build.rs
    Butt in teh end

--- build.rs
+++ build.rs
@@ -8,11 +8,11 @@ fn main() {
      cc::Build::new()
         .compiler("/usr/bin/clang")
         .no_default_flags(true)
-        .file("./webpwrapper/webwrapper.c")
+        .file("src//webwrapper.c")
         .pic(true)
         .static_flag(true)
         .opt_level(3)
-        .include("./deps/include")
-        .compile("./deps/lib/libwebpwrapper.a");
-    println!("cargo:rustc-link-search=./deps/lib");
+        .include("deps/include")
+        .compile("lib/libwebpwrapper.a");
+    println!("cargo:rustc-link-search=lib");
 }
