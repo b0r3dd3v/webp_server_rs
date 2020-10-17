908f7dd5eac0fec8d38b02c81757363da1dd4453
Goodyeah <44746806+b0r3dd3v@users.noreply.github.com>
2020-10-17 23:22:20 +0300

    Update build.rs
    Getting rid of cmake succescode just 4 single haloworld.

--- build.rs
+++ build.rs
@@ -1,3 +1,18 @@
+use std::path::Path;
 fn main() {
+    
+     let clang = Path::new("/usr/bin/clang");
+     let fille = Path::new("./webpwrapper/webwrapper.c");
+     let inclu3de = Path::new("./deps/include");
+     let depthlib = Path::new("./deps/lib/libwebpwrapper.a");
+     cc::Build::new()
+        .compiler(clang)
+        .no_default_flags(true)
+        .file(fille)
+        .pic(true)
+        .static_flag(true)
+        .opt_level(3)
+        .include(inclu3de)
+        .compile(depthlib);
     println!("cargo:rustc-link-search=./deps/lib");
 }
